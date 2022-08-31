ed73f5d68f4b42ecf7e91a26dd590abaf99ce898
hancheng <215842894@qq.com>
2022-08-31 05:46:35 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import gradio as gr
 import os
 
-#os.system("wget https://github.com/WongKinYiu/yolov7/releases/download/v0.1/yolov7.pt")
+os.system("wget https://github.com/WongKinYiu/yolov7/releases/download/v0.1/yolov7.pt")
 #os.system("wget https://github.com/WongKinYiu/yolov7/releases/download/v0.1/yolov7-e6e.pt")
 #os.system("wget https://github.com/WongKinYiu/yolov7/releases/download/v0.1/yolov7-e6.pt")
 os.system("wget https://github.com/CAIC-AD/YOLOPv2/releases/download/V0.0.1/yolopv2.pt")
