d3535f4aed30891db90aaf1e5f6c9963b4bbdd09
hancheng <215842894@qq.com>
2022-09-01 03:00:37 +0000

    Update lib/models/YOLOP.py

--- lib/models/YOLOP.py
+++ lib/models/YOLOP.py
@@ -13,9 +13,19 @@ sys.path.append(os.getcwd())
 # from lib.models.common2 import SPP, Bottleneck, BottleneckCSP, Focus, Concat, Detect
 from lib.models.common import Conv, SPP, Bottleneck, BottleneckCSP, Focus, Concat, Detect, SharpenConv
 from torch.nn import Upsample
-from lib.utils import check_anchor_order
+#from lib.utils import check_anchor_order
 from lib.core.evaluate import SegmentationMetric
 #from lib.utils.utils import time_synchronized
+
+def check_anchor_order(m):
+    # Check anchor order against stride order for YOLOv5 Detect() module m, and correct if necessary
+    a = m.anchor_grid.prod(-1).view(-1)  # anchor area
+    da = a[-1] - a[0]  # delta a
+    ds = m.stride[-1] - m.stride[0]  # delta s
+    if da.sign() != ds.sign():  # same order
+        print('Reversing anchor order')
+        m.anchors[:] = m.anchors.flip(0)
+        m.anchor_grid[:] = m.anchor_grid.flip(0)
 
 """
 MCnet_SPP = [
