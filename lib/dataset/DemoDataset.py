e09d442691691f8226485790e0a365f1ce4b2a31
hancheng <215842894@qq.com>
2022-09-01 06:22:48 +0000

    Update lib/dataset/DemoDataset.py

--- lib/dataset/DemoDataset.py
+++ lib/dataset/DemoDataset.py
@@ -110,7 +110,7 @@ class LoadImages:  # for inference
                     path = self.files[self.count]
                     self.new_video(path)
                     ret_val, img0 = self.cap.read()
-            h0, w0 = img0.shape[:2]
+        
 
             self.frame += 1
             print('\n video %g/%g (%g/%g) %s: ' % (self.count + 1, self.nf, self.frame, self.nframes, path), end='')
@@ -122,9 +122,11 @@ class LoadImages:  # for inference
             #img0 = cv2.cvtColor(img0, cv2.COLOR_BGR2RGB)
             assert img0 is not None, 'Image Not Found ' + path
             print('image %g/%g %s: \n' % (self.count, self.nf, path), end='')
-            h0, w0 = img0.shape[:2]
+       
 
         # Padded resize
+        h0, w0 = img0.shape[:2]
+        img0 = cv2.resize(img0,(1280,720),interpolation = cv2.INTER_LINEAR)
         img, ratio, pad = letterbox_for_img(img0, new_shape=self.img_size, auto=True)
         h, w = img.shape[:2]
         shapes = (h0, w0), ((h / h0, w / w0), pad)
